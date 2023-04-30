45a764d155ed3914b06d4c9d7bc85cb23e1ca903
m.safi <m.safi@mapletele.com>
2023-04-30 16:55:13 +0300

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        click me
+    </div>
+</template>
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTimer: 0
+        }
+    },
+    mounted() { // mount hook
+        console.log(`component mounted`)
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+            console.log(`delay: ${this.delay}`)
+        }, this.delay);
+    },
+    updated() { // updated hook
+        console.log(`component updated`)
+    },
+    unmounted() { // unmounted hook
+        console.log(`component unmounted`)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTimer += 10
+            }, 10);
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(`reactionTimer: ${this.reactionTimer}`)
+            this.$emit('end', this.reactionTimer)
+        }
+    }
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
