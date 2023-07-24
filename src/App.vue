ef7da3ded09f617fb6ae26d53e869ebbc7a0890a
m.safi <m.safi@mapletele.com>
2023-07-24 13:58:47 +0300

    add modal

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-   <h1>Ninja Reaction Timer</h1>
-   <button @click="start" :disabled="isPlaying">play</button>
-   <Block v-if="isPlaying" :delay="delay" @end="endGame"></Block>
-   <Results v-if="showResults" :score="score"></Results>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"></Block>
+  <Results v-if="showResults" :score="score"></Results>
+  <div v-if="showModal">
+    <Modal :exp1="exp1" @close="toggleModal">
+      <h1>Hiii h1</h1>
+      <p>this is MS</p>
+      <template v-slot:links><a href="#">link 1</a></template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
 import Block from './components/Block.vue'
 import Results from './components/Results.vue'
+import Modal from './components/Modal.vue'
 export default {
   name: 'App',
-  components: { Block, Results },
+  components: { Block, Results, Modal },
   data() {
     return {
       isPlaying: false,
       delay: null,
       score: null,
-      showResults: false
+      showResults: false,
+      exp1: 'Hi Modal',
+      showModal: false
     }
   },
   methods: {
     start() {
-      this.delay = 2000 + (Math.random()* 5000)
+      this.delay = 2000 + (Math.random() * 5000)
       this.isPlaying = true
       this.showResults = false
       console.log(`delay No: ${this.delay}`)
@@ -30,6 +41,9 @@ export default {
       this.score = reactionTimer
       this.isPlaying = false
       this.showResults = true
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
     }
   }
 }
@@ -44,6 +58,7 @@ export default {
   color: #444;
   margin-top: 60px;
 }
+
 button {
   background: #0faf87;
   color: white;
@@ -55,6 +70,7 @@ button {
   cursor: pointer;
   margin: 10px;
 }
+
 button[disabled] {
   opacity: 0.2;
   cursor: not-allowed;
---
+++ src/components/Modal.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal">
+            <h3>{{ exp1 }}</h3>
+            <slot>defualt content</slot>
+            <slot name="links"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['exp1'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style>
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+
+}
+</style>
